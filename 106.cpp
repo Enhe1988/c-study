c62ac828ef040f4a5f175740e9d264410d92f16f
Enhe <1352611731@qq.com>
2023-01-11 00:18:23 +0800

    106完结

--- 106.cpp
+++ 106.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
+class person
+{
+   public:
+person()
+{
+   cout<<"构造函数调用"<<endl;
+}
+~person()
+{
+   cout<<"析构函数调用"<<endl;
+}
+};
+void test01()
+{
+ person p;
+}
 int main()
 {
+  //test01();
+ person p;
+ system("pause");
    return 0;    
 }
